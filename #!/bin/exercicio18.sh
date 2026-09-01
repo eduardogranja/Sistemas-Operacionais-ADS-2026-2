35e718a7d4c7125b8474915d0e6311fbca8d8208
eduardogranja <egranjaa@gmail.com>
2026-09-01 11:55:30 -0300

    Painel de informacoes do sistema

---
+++ #!/bin/exercicio18.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "================ PAINEL DO SISTEMA ================"
+
+echo -e "\n1. Data e Hora Atual:"
+date
+
+echo -e "\n2. Versão do Kernel:"
+uname -r
+
+echo -e "\n3. Calendário do Mês Atual:"
+cal
+
+echo -e "\n4. Tempo de Funcionamento (Uptime):"
+uptime
+
+echo "==================================================="
