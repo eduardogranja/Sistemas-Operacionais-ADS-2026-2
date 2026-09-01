77961bbc45538c0778a40696b82fffbb2d6e61fc
eduardogranja <egranjaa@gmail.com>
2026-09-01 11:51:37 -0300

    Listar processos

---
+++ #!/bin/exercicio13.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "=========================================================="
+echo "Exibindo todos os processos em execução no sistema abaixo:"
+echo "=========================================================="
+sleep 1
+
+ps aux
